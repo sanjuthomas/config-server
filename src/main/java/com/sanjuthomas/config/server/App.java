090971f4c7d883c8fb0fbd9a0495ee42a0818184
sanjuthomas <sanju@sanju.org>
2020-04-26 00:35:42 +0530

    adding the search property

--- src/main/java/com/sanjuthomas/config/server/App.java
+++ src/main/java/com/sanjuthomas/config/server/App.java
@@ -1,7 +1,16 @@
 package com.sanjuthomas.config.server;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
 public class App {
 
-
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
 
 }
